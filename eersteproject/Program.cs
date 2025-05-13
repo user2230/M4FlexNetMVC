86bb5461f9707d81ddbff84084e98df52519b0a3
user2230 <furkanefeceylan2008@hotmail.com>
2025-05-13 12:21:01 +0200

    first

---
+++ eersteproject/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int number = 0;
+string name = "Hello";
+bool istrue = true;
+float pi = 3.14f;
+
+Console.WriteLine(number);
+Console.WriteLine(name);
+Console.WriteLine(istrue);
+Console.WriteLine(pi);
